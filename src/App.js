e8a37c7382c872031dadcda821efc8f96c1463b8
mumtazkakakhail <mumtazkakakhail@gmail.com>
2025-03-28 21:28:39 +0500

    Protfolio

--- src/App.js
+++ src/App.js
@@ -1,13 +1,35 @@
+<<<<<<< HEAD
 // import logo from './logo.svg';
 import './App.css';
 import ResponsiveAppBar from './components/Navbar';
 import HomePage from './components/HomePage';
+=======
+import logo from './logo.svg';
+import './App.css';
+>>>>>>> a649691 (Initialize project using Create React App)
 
 function App() {
   return (
     <div className="App">
+<<<<<<< HEAD
       <ResponsiveAppBar/>
       <HomePage/>
+=======
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+>>>>>>> a649691 (Initialize project using Create React App)
     </div>
   );
 }
