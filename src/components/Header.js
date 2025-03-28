c09bb46ed2b948c5da2112d8395b0d972c8a3bad
mumtazkakakhail <mumtazkakakhail@gmail.com>
2025-03-28 21:12:34 +0500

    Protfolio using React js

---
+++ src/components/Header.js
@@ -0,0 +1,24 @@
+import React from "react";
+import "./Header.css"
+import Logo from "./images/bg.png"; // Assuming you have a logo image in the assets folder
+
+function header(){
+    return (
+        <div className="header">
+            <div className="logo-txt">
+            <img src={Logo} alt="logo" />
+            <h1>My Portfolio</h1>
+            </div>
+            <nav>
+                <ul className="nav-links">
+                    <li><a href="#home">Home</a></li>
+                    <li><a href="#about">About Me</a></li>
+                    <li><a href="#projects">Projects</a></li>
+                    <li><a href="#contact">Contact</a></li>
+                </ul>
+            </nav>
+        </div>
+    )
+}
+
+export default header;
---
+++ src/components/Homepage.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Homepage.css";
+
+function Homepage() {
+  return (
+    <div className="homepage">
+    <span className="span">Hello,</span>
+    <h1>I'm, <span className="name">Mumtaz Wali</span></h1>
+    <span className="span">A Passionate</span>
+    <h1>Web Developer</h1>
+    </div>
+
+  );
+}
+export default Homepage;
